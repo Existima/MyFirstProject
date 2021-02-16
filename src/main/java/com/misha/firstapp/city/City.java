21396282bafef94d94a11bd3d263fb8d96570add
Volodymyr Bodnar <vbodnar@pl.sii.eu>
2021-02-16 16:06:47 +0100

    Domashka test

--- src/main/java/com/misha/firstapp/city/City.java
+++ src/main/java/com/misha/firstapp/city/City.java
@@ -4,17 +4,53 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class City {
-    private Map<String, Integer> citizens = new HashMap();
+    /**
+     * Citizens of a city, key is the name, value is age
+     */
+    private Map<String, Integer> citizens = new HashMap<>();
 
     public void addCitizen(String name, int age) {
         citizens.put(name, age);
     }
 
+    /**
+     * Returns number of citizens in the city
+     * @return
+     */
     public int getCitizensCount() {
-        return citizens.size();
+        return 0; //todo
     }
 
+    /**
+     * Returns oldest citizen in the city
+     * @return the oldest citizen in the City in format where key is name, value is age
+     */
     public Map.Entry<String, Integer> getOldestCitizen() {
-        return null;
+        return null; //todo
+    }
+
+    /**
+     * Returns citizens within age groups:
+     * CHILDREN age 0-18
+     * ADULTS age 18-65
+     * RETIREES age 65 - max integer
+     * @return returns Map of citizens
+     */
+    public Map<Group, Map<String, Integer>> getAgeGroups(){
+        return null; //todo
+    }
+
+    enum Group{
+        CHILDREN(0, 18),
+        ADULTS(18,65),
+        RETIREES(65, Integer.MAX_VALUE);
+
+        private final int minAge;
+        private final int maxAge;
+
+        Group(int minAge, int maxAge) {
+            this.minAge = minAge;
+            this.maxAge = maxAge;
+        }
     }
 }
--- src/test/java/com/misha/firstapp/city/CityTest.java
+++ src/test/java/com/misha/firstapp/city/CityTest.java
@@ -2,7 +2,10 @@ package com.misha.firstapp.city;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CityTest {
     @Test
@@ -18,5 +21,48 @@ public class CityTest {
 
         //then
         assertEquals(3, citizenCount);
+    }
+
+    @Test
+    public void oldestCitizenTest() {
+        //given
+        City testCity = new City();
+        testCity.addCitizen("Bilbo", 24);
+        testCity.addCitizen("Gendalf", 305);
+        testCity.addCitizen("Frodo", 3);
+
+        //when
+        Map.Entry<String, Integer> citizen = testCity.getOldestCitizen();
+
+        //then
+        assertEquals("Gendalf", citizen.getKey());
+        assertEquals(305, citizen.getValue());
+    }
+
+    @Test
+    public void getAgeGroupsTest() {
+        //given
+        City testCity = new City();
+        testCity.addCitizen("Bilbo", 24);
+        testCity.addCitizen("Gendalf", 305);
+        testCity.addCitizen("Arven", 295);
+        testCity.addCitizen("Frodo", 3);
+        testCity.addCitizen("Aragorn", 45);
+
+        //when
+        Map<City.Group, Map<String, Integer>> groups = testCity.getAgeGroups();
+
+        //then
+        assertEquals(3, groups.size());
+
+        assertEquals(1, groups.get(City.Group.CHILDREN).size());
+        assertEquals(2, groups.get(City.Group.ADULTS).size());
+        assertEquals(2, groups.get(City.Group.RETIREES).size());
+
+        assertTrue(groups.get(City.Group.CHILDREN).containsKey("Frodo"));
+        assertTrue(groups.get(City.Group.ADULTS).containsKey("Bilbo"));
+        assertTrue(groups.get(City.Group.ADULTS).containsKey("Aragorn"));
+        assertTrue(groups.get(City.Group.RETIREES).containsKey("Gendalf"));
+        assertTrue(groups.get(City.Group.RETIREES).containsKey("Arven"));
     }
 }
